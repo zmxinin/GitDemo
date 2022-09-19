51882fd4c58bfbd4fe7e0ed658a49794fd170738
zmxinin <751549603@qq.com>
2022-09-19 14:15:33 +0800

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * 功能描述
+ *
+ * @author zmx
+ * @date 2022/9/19 14:11
+ */
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
